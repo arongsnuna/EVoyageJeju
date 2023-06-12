ac9a7a8d271343fd4cd27d9748dce7e0963a91cc
[AI7] 강다영 <wkvksrl100@gmail.com>
2023-06-12 15:14:17 +0900

    feat: 게시판 글목록 구현 및 스타일 추가

--- front/src/pages/Community/Community.jsx
+++ front/src/pages/Community/Community.jsx
@@ -1,15 +1,94 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Container, TitleContainer, TypeContainer, TypeButton, IndexContainer, ListContainer } from "./Community.style";
 
-import { Container } from "./Community.style";
+const Community = () => {
+  const [activeTab, setActiveTab] = useState('travel');
+  const [posts, setPosts] = useState([
+    // 여행 게시글
+    { id: 1, title: '게시글 1', type: '여행', author: '작성자1', date: '2023-06-01', likes: 10 },
+    { id: 2, title: '게시글 2', type: '여행', author: '작성자2', date: '2023-06-02', likes: 5 },
+    // 전기차 게시글
+    { id: 3, title: '게시글 3', type: '전기차', author: '작성자3', date: '2023-06-03', likes: 7 },
+    { id: 4, title: '게시글 4', type: '전기차', author: '작성자4', date: '2023-06-04', likes: 12 },
+  ]);
+  const [posting, setPosting] = useState(false)
 
-function Community() {
+  // const handleTravelTab = async () => {
+  //   const res = await Api.get('/community/:postingId');
+  //   setPosts(res.data)
+  //   setActiveTab('travel')
+  // }
+
+  // const handleElecTab = async () => {
+  //   const res = await Api.get('/community/:postingId');
+  //   setPosts(res.data)
+  //   setActiveTab('electricCar')
+  // }
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+  
+  // const filteredPosts = posts.filter((post) => {
+  //   if (activeTab === 'travel') {
+  //     return post.title.startsWith('여행');
+  //   } else if (activeTab === 'electricCar') {
+  //     return post.title.startsWith('전기차');
+  //   }
+  //   return false;
+  // });
 
   return (
     <Container>
-      <h1>여기에서 Modal??을 통해 탭으로 여행/전기차 구분</h1>
-      <h1>게시글, 좋아요, 댓글 등등...</h1>
+      <TitleContainer>
+        <p>게시판</p>
+      </TitleContainer>
+      <TypeContainer>
+        <div>
+          <TypeButton 
+            fontColor='#21272A'
+            onClick={() => handleTabChange('travel')}
+          >여행탭
+          </TypeButton>
+          <TypeButton
+            fontColor='#3563E9'
+            onClick={() => handleTabChange('electricCar')}
+          >전기차탭
+          </TypeButton>
+        </div>
+      </TypeContainer>
+      <div>
+        <IndexContainer>
+          <div>
+            <p className='index'>글 번호</p>
+            <p className='title'>제목</p>
+            <p className='author'>글쓴이</p>
+            <p className='date'>등록일</p>
+            <p className='likeCount'>좋아요</p>
+          </div>
+        </IndexContainer>
+        <ListContainer>
+          <div>
+            {posts.map((post) => (
+              <div key={post.id}>
+                <p className='index'>{post.id}</p>
+                <p className='title'>{post.title}</p>
+                <p className='author'>{post.author}</p>
+                <p className='date'>{post.date}</p>
+                <p className='likeCount'>{post.likes}</p>
+              </div>
+            ))}
+          </div>
+        </ListContainer>
+      </div>
+      <div>
+        <button>글쓰기</button>
+      </div>
+      <div>
+        {/* 페이징 넘버링 추가필요 */}
+      </div>
     </Container>
   );
-}
+};
 
 export default Community;
+
--- front/src/pages/Community/Community.style.jsx
+++ front/src/pages/Community/Community.style.jsx
@@ -3,3 +3,130 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: 170px 0 800px;
 `;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 230px;
+
+  p {
+    padding: 0 30px 35px;
+    width: 2300px;
+
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 60px;
+    line-height: 110%;
+    text-align: center;
+    color: #21272A;
+  }
+`;
+
+export const TypeContainer =  styled.div`
+  display: flex;
+  justify-content: center;
+
+  div {
+    width: 2316px;
+    border-bottom: 3px solid #DDE1E6;
+  }
+`;
+
+export const TypeButton = styled.button`
+  cursor: pointer;
+  padding: 40px 85px;
+  border: none;
+  background: none;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 28px;
+  line-height: 100%;
+  letter-spacing: 0.5px;
+  color: ${(props) => props.fontColor};
+
+  &:hover {
+    background: #A6C8FF;
+  }
+`;
+
+export const IndexContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 550;
+  font-size: 26px;
+  line-height: 110%;
+  color: #121619;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    background: #F2F4F8;
+
+    p {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      height: 40px;
+      margin: 0;
+      padding: 10px 0;
+      border: 1px solid #DDE1E6;
+    }
+
+    .index { width: 150px; }
+
+    .title { width: 1200px;}
+
+    .author { width: 400px; }
+
+    .date { width: 400px; }
+
+    .likeCount { width: 150px; }
+  }
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-size: 26px;
+  line-height: 110%;
+  color: #121619;
+
+  div > div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    p {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      height: 40px;
+      margin: 0;
+      padding: 10px 0;
+      border: 1px solid #DDE1E6;
+    }
+
+    .index { width: 150px; }
+    .title { width: 1200px;}
+    .author { width: 400px; }
+    .date { width: 400px; }
+    .likeCount { width: 150px; }
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  
+`;
