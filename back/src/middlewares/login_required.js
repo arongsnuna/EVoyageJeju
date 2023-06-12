1d54a57f03b37db5cbe44eb2e36036429ffbb79f
arongsnuna <unhi000@naver.com>
2023-06-12 14:20:53 +0900

    fix: login_required에서 bearer token 검증 방법 수정

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -1,25 +1,27 @@
 import jwt from 'jsonwebtoken';
+function verifyAuthorizationHeader(authorizationHeader) {
+    if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+      return true; // Bearer 토큰이 맞음을 확인
+    } else {
+      return false; // Bearer 토큰이 아님
+    }
+  }
 
 function login_required(req, res, next) {
+    try{
+        const authorizationHeader = req.headers.authorization;
+        const isBearerToken = verifyAuthorizationHeader(authorizationHeader);
+
+        if (!isBearerToken) {
+            throw new Error('BearerToken이 아닙니다.')
+        } 
+    }
+    catch(error){
+        next(error);
+    }
+
     // request 헤더로부터 authorization bearer 토큰을 받음.
     const userToken = req.headers['authorization']?.split(' ')[1] ?? 'null';
-
-    //bearer 토큰인지 검증
-    // 토큰 디코딩
-    const decoded = jwt.decode(userToken , { complete: true });
-    // 디코딩된 페이로드에서 Authorization 헤더의 값 확인
-    const authorizationHeader = decoded.payload.Authorization;
-    if (!authorizationHeader.startsWith('Bearer')) {
-        res.status(401).send('bearer 토근이 아닙니다.');
-    }
-    
-    // 이 토큰은 jwt 토큰 문자열이거나, 혹은 "null" 문자열임.
-    // 토큰이 "null" 일 경우, login_required 가 필요한 서비스 사용을 제한함.
-    if (userToken === 'null') {
-        console.log('서비스 사용 요청이 있습니다.하지만, Authorization 토큰: 없음');
-        res.status(400).send('로그인한 유저만 사용할 수 있는 서비스입니다.');
-        return;
-    }
 
     // 해당 token 이 정상적인 token인지 확인 -> 토큰에 담긴 userId 정보 추출
     try {
