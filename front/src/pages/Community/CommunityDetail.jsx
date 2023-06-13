1405818f7e669af06c8868ed124cf88f96f1d187
[AI7] 강다영 <wkvksrl100@gmail.com>
2023-06-13 17:25:50 +0900

    style: 작성페이지, 상세페이지 스타일 구현

--- front/src/pages/Community/CommunityDetail.jsx
+++ front/src/pages/Community/CommunityDetail.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { ROUTE } from '../../routes';
+import { Container, TitleContainer, TypeContainer, TypeButton, ContentContainer, ButtonContainer } from './CommunityDetail.style';
 
 const CommunityDetail = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const { title, author, date, content, likeCount } = location.state;
- 
+  const { title = '제목입니다', author = '제주도전기차', date = '2023-05-06', content = '내용입니다', likeCount = 0 } = location.state || {};
 
   const handleEdit = () => {
     // 수정 기능 구현
     console.log('Edit');
-    () => navigate(ROUTE.COMMUNITYEdit.link);
+    navigate(ROUTE.COMMUNITYEdit.link);
   };
 
   const handleDelete = () => {
@@ -26,24 +26,53 @@ const CommunityDetail = () => {
 
   const handleList = () => {
     // 목록으로 이동
-    () => navigate(ROUTE.COMMUNITY.link);
+    navigate(ROUTE.COMMUNITY.link);
   };
 
   return (
-    <div>
-      <h2>게시글 상세 페이지</h2>
-      <div>
-        <h3>제목: {title}</h3>
-        <p>글쓴이: {author}</p>
-        <p>등록일: {date}</p>
-        <p>좋아요 수: {likeCount}</p>
-        <p>본문 내용: {content}</p>
-        <button onClick={handleEdit}>수정</button>
-        <button onClick={handleDelete}>삭제</button>
-        <button onClick={handleLike}>좋아요</button>
-        <button onClick={handleList}>목록</button>
-      </div>
-    </div>
+    <Container>
+      <TitleContainer>
+        <p>게시판</p>
+      </TitleContainer>
+      <TypeContainer>
+        <div>
+          <TypeButton 
+            fontColor='#21272A'
+            // onClick={() => handleTabChange('travel')}
+          >여행탭
+          </TypeButton>
+          <TypeButton
+            fontColor='#3563E9'
+            // onClick={() => handleTabChange('electricCar')}
+          >전기차탭
+          </TypeButton>
+        </div>
+      </TypeContainer>
+      <ContentContainer>
+        <div className='title-box'>
+          <div>{title}</div>
+        </div>
+        <div className='posting-infobox'>
+          <div className='info'>글쓴이</div>
+          <div>{author}</div>
+          <div className='info'>등록일</div>
+          <div>{date}</div> 
+          <div className='info'>좋아요 수</div>
+          <div>{likeCount}</div>
+        </div>
+        <div className='content-box'>
+          <div>{content}</div>
+        </div>
+      </ContentContainer>
+      <ButtonContainer>
+        <div>
+          <button onClick={handleList}>목록</button>
+          <button onClick={handleEdit}>수정</button>
+          <button className='delete' onClick={handleDelete}>삭제</button>
+          <button className='like' onClick={handleLike}>좋아요</button>
+        </div>
+      </ButtonContainer>
+    </Container>
   );
 };
 
--- front/src/pages/Community/CommunityWrite.jsx
+++ front/src/pages/Community/CommunityWrite.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { Container } from './CommunityWrite.style';
+import { Container, TitleContainer, InputContainer, RadioContainer, ContentContainer, ButtonContainer } from './CommunityWrite.style';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE } from '../../routes';
 
 const CommunityWrite = () => {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState('뚜룹');
   const [content, setContent] = useState('');
   const navigate = useNavigate();
+
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
@@ -15,45 +16,52 @@ const CommunityWrite = () => {
     setContent(event.target.value);
   };
 
+  const handleList = () => {
+    // 목록으로 이동
+    navigate(ROUTE.COMMUNITY.link);
+  };
+
   const handleSave = () => {
     // TODO: 글 저장 로직 추가
     console.log('Title:', title);
     console.log('Content:', content);
     // 저장 로직을 추가하고, 필요한 경우 서버로 데이터를 보내거나 상태를 업데이트하세요.
     // 저장 후 필요한 작업을 수행하고, 예를 들어 글 목록 페이지로 이동할 수 있습니다.
-    () => navigate(ROUTE.COMMUNITYDetail.link);
+    navigate(ROUTE.COMMUNITYDetail.link);
   };
 
   return (
     <Container>
-      <div>
-        <div>
-          <label>
+      <TitleContainer>
+        <p>게시판</p>
+      </TitleContainer>
+      <InputContainer>
+        <RadioContainer>
+          <p>유형</p>
+          <div>
             <input type="radio" value="electricCar" name="tab" />
-            전기차탭
-          </label>
-          <label>
+            <label>전기차탭</label>
+          </div>
+          <div>
             <input type="radio" value="travel" name="tab" />
-            여행탭
-          </label>
-        </div>
-        <div>
-          <label>
-            제목:
-          </label>
+            <label>여행탭</label>
+          </div>
+        </RadioContainer>
+        <ContentContainer>
+          <div>
+            <label>제목</label>
             <input type="text" value={title} onChange={handleTitleChange} />
-        </div>
-        <div>
-          <label>
-            본문:
-          </label>
+          </div>
+          <div>
+            <label>본문</label>
             <textarea value={content} onChange={handleContentChange} />
-          
-        </div>
-        <div>
+          </div>
+        </ContentContainer>
+        <ButtonContainer>
+          <button onClick={handleList}>목록</button>
           <button onClick={handleSave}>저장</button>
-        </div>
-      </div>
+        </ButtonContainer>
+      </InputContainer>
     </Container>
   );
 };
