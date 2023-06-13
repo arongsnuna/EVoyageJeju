b3d511ee65279a67605773a80e7e097fa8bf2ee7
[AI7] 이원규 <leewongyue@gmail.com>
2023-06-13 10:57:19 +0000

    update: 주석 변경

--- back/src/services/likeService.js
+++ back/src/services/likeService.js
@@ -2,7 +2,13 @@ import {pool} from'../config/dbConnect.js';
 
 class likeService{
 
-    // 해당 게시글의 모든 댓글 가져오기
+    /**
+     * params: 
+     *      postId = 게시글 아이디
+     *  description:
+     *      게시글 아이디로 모든 좋아요 수를 조회합니다.
+     */
+
     static async getPostLikeCount({postId}){
         return new Promise((resolve, reject)=>{
             const sql = `select * from PostLikeCount where postId = '${postId}'`;
@@ -18,7 +24,14 @@ class likeService{
             })
         })
     }
-    // 해당 글에 유저가 좋아요를 눌렀는지 확인
+    /**
+     * params: 
+     *      postId = 게시글 아이디
+     *      userId = 유저 아이디
+     *  description:
+     *      특정 유저가 좋아요를 눌렀는지 확인합니다.    
+     */
+
     static async checkPostLikeCount({postId, userId}){
         return new Promise((resolve, reject)=>{
             const sql = `select * from LikeCount where postId = '${postId}' and userId = '${userId}'`;
@@ -34,7 +47,13 @@ class likeService{
             })
         })
     }
-    // 특정 유저가 좋아요를 누른 모든 글 조회
+    /**
+     * params: 
+     *      userId = 유저 아이디
+     *  description:
+     *      특정 유저가 좋아요를 누른 모든 게시글을 조회합니다.
+     */
+
     static async getUserLikeCount({userId}){
         return new Promise((resolve, reject)=>{
             const sql = `select * from LikeCount where userId = '${userId}'`;
@@ -53,7 +72,14 @@ class likeService{
     }
 
     
-    // 특정 글의 좋아요 카운트 삭제
+    /**
+     * params: 
+     *      postId = 게시글 아이디
+     *      userId = 유저 아이디
+     *  description:
+     *      좋아요를 삭제합니다.  
+     */
+
     static async decrementLikeCount({postId, userId}){
         return new Promise((resolve, reject)=>{
             const sql = `DELETE from LikeCount WHERE postId = '${postId}' and userId = '${userId}'`;
@@ -70,7 +96,14 @@ class likeService{
         })
     }
 
-    // 특정 글의 좋아요 증가
+    /**
+     * params: 
+     *      postId = 게시글 아이디
+     *      userId = 유저 아이디
+     *  description:
+     *       좋아요를 추가합니다.  
+     */
+
     static async incrementLikeCount({ postId, userId }){
         return new Promise((resolve, reject)=>{
             const sql = `insert into LikeCount(userId,postId) values ('${userId}','${postId}')`;
