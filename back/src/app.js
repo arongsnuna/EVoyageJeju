d606ab357e7e6efbf027b154a17d8cb00acea606
arongsnuna <unhi000@naver.com>
2023-06-15 20:36:03 +0900

    fix: body-parser 미들웨어를 사용하여 요청 본문의 크기 제한을 변경

--- back/src/app.js
+++ back/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '10mb', extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello express");
